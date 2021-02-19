163a12618b1e4939df68ec96b60b272da8eb6a04
Matija <matija.rudnik@convergent-usa.com>
2021-02-19 09:47:54 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const correctAnswers = ['B', 'B', 'B', 'B']
+const form = document.querySelector('.quiz-form')
+const resultContainer = document.querySelector('.result')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
+
+    scrollTo(0, 0);
+
+    resultContainer.classList.remove('d-none');
+
+    let output = 0;
+
+    const timer = setInterval(() => {
+        resultContainer.querySelector('span').textContent = `${output}%`;
+
+        if (output === score) {
+            clearInterval(timer);
+        } else {
+            output++
+        }
+    }, 10);
+});
